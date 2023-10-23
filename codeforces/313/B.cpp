edd9e8254c42bf72e12df8acb06725373ae6b298
abdullahazmy <abdullahazmy2@gmail.com>
2023-10-23 21:29:00 +0300

    Add solution for problem `B - Ilya and Queries` Link: https://codeforces.com/contest/313/problem/B Tags: dp, implementation, *1100 Ref: https://codeforces.com/contest/313/submission/229470540

--- codeforces/313/B.cpp
+++ codeforces/313/B.cpp
@@ -1,38 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-#define new "\n";
-#define ll long long
-void input()
-{
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-#endif
-}
-string enter;
-int main()
-{
-    fast
-    input();
-    int m, l, r, cnt = 0;
+
+main(){
+    int m,l,r,cnt=0;
 
     int dp[100005];
     string s;
 
-    cin >> s >> m;
+    cin>>s>>m;
 
-    for (int i = 1; i < s.size(); i++)
-    {
+    for(int i=1;i<s.size();i++){
 
-        if (s[i] == s[i - 1])
+        if(s[i]==s[i-1])
             cnt++;
 
-        dp[i] = cnt;
+        dp[i]= cnt;
+
     }
 
-    while (m--)
-    {
-        cin >> l >> r;
-        cout << dp[r - 1] - dp[l - 1] << new;
+    while(m--){
+        cin>>l>>r;
+        cout<<dp[r-1]-dp[l-1]<<endl;
     }
+
 }
