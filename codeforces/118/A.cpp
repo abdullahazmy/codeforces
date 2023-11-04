ec0e5587cbb9a7106e4a385b998dd3ef3808b02e
abdullahazmy <abdullahazmy2@gmail.com>
2023-11-05 01:52:00 +0200

    Add solution for problem `A - String Task` Link: https://codeforces.com/contest/118/problem/A Tags: implementation, strings, *1000 Ref: https://codeforces.com/contest/118/submission/231399683

---
+++ codeforces/118/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define new "\n"
+#define ll long long
+
+void input()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+}
+class Rectangle
+{
+private:
+};
+int main()
+{
+    fast
+    input();
+    string s, t;
+    cin >> s;
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y')
+            continue;
+        else
+        {
+            t += '.';
+            t += s[i];
+        }
+    }
+
+    cout << t;
+}
